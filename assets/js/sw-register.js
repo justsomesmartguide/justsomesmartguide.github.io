65a96715722570ecc702b535a369ee721b1e1e1f
justsomesmartguide <228777278+justsomesmartguide@users.noreply.github.com>
2025-09-03 22:33:34 +0100

    Delete assets/js/sw-register.js

--- assets/js/sw-register.js
+++
@@ -1,7 +0,0 @@
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./sw.js')
-      .then(reg => console.log('Service Worker registered:', reg.scope))
-      .catch(err => console.error('Service Worker registration failed:', err));
-  });
-}
